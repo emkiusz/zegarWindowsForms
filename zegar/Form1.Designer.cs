6eac63aba8db74590bd088e3566d0e6e3c646f2b
Marcin-Komputer\Marcin <mkiszka525@gmail.com>
2020-05-31 22:04:40 +0200

    Initial commit

---
+++ zegar/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace zegar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Włącz zegar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(284, 362);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zegar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ zegar/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zegar
+{
+    public partial class Form1 : Form
+    {
+        int x, y;
+        Timer t = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Location = new Point(10, ClientSize.Height - 30);
+            label1.Location = new Point(ClientSize.Width/2-60, ClientSize.Height/2);
+            label1.Text = "ZEGAR WYŁĄCZONY";
+            t.Interval = 1000;
+            t.Tick += new EventHandler(timer1_Tick);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button1.Location = new Point(10, ClientSize.Height-30);
+
+            int size = ClientSize.Width;
+            if(size>ClientSize.Height)
+            {
+                size = ClientSize.Height;
+            }
+            x = size / 2;
+            y = size / 2;
+            Graphics g = this.CreateGraphics();
+            Pen p = new Pen(Color.Black, 1f);
+            Point srod = new Point(x, y);
+            label1.Location = new Point(size / 2-24,40); 
+
+            int sek = DateTime.Now.Second;
+            int min = DateTime.Now.Minute;
+            int godz = DateTime.Now.Hour;
+
+            if (sek < 10)
+            {
+                label1.Text = godz + ":" + min + ":" + "0"+sek;
+            }
+            else
+            {
+                label1.Text = godz + ":" + min + ":" + sek;
+            }
+
+            int[] koniec_kordy = new int[2];
+
+            g.Clear(Color.White);
+
+            g.DrawEllipse(p, 0, 0, size, size);
+
+            koniec_kordy = sek_kordy(sek, size / 3+30);
+            g.DrawLine(p, srod, new Point(koniec_kordy[0], koniec_kordy[1]));
+
+            koniec_kordy = sek_kordy(min, size/4+20);
+            g.DrawLine(p, srod, new Point(koniec_kordy[0], koniec_kordy[1]));
+
+            koniec_kordy = godz_kordy(godz % 12, min, size/5+20);
+            g.DrawLine(p, srod, new Point(koniec_kordy[0], koniec_kordy[1]));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t.Start();
+        }
+
+        private int[] sek_kordy(int val, int dl)
+        {
+            int[] kordy = new int[2];
+            val *= 6;
+
+            if (val >= 0 && val <= 180)
+            {
+                kordy[0] = x + (int)(dl * Math.Sin(Math.PI * val / 180));
+                kordy[1] = y - (int)(dl * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                kordy[0] = x - (int)(dl * -Math.Sin(Math.PI * val / 180));
+                kordy[1] = y - (int)(dl * Math.Cos(Math.PI * val / 180));
+            }
+            return kordy;
+        }
+
+        private int[] godz_kordy(int hval, int mval, int dl)
+        {
+            int[] kordy = new int[2];
+
+            int val = (int)((hval * 30) + (mval * 0.5));
+
+            if (val >= 0 && val <= 180)
+            {
+                kordy[0] = x + (int)(dl * Math.Sin(Math.PI * val / 180));
+                kordy[1] = y - (int)(dl * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                kordy[0] = x - (int)(dl * -Math.Sin(Math.PI * val / 180));
+                kordy[1] = y - (int)(dl * Math.Cos(Math.PI * val / 180));
+            }
+            return kordy;
+        }
+    }
+}
